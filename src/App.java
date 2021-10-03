eaa4e4e06e41f240f6d67191a0b789a35087dd85
Jules Nicolas-Thouvenin <40352310+jnicolasthouvenin@users.noreply.github.com>
2021-10-03 14:02:43 +0200

    Small test loop

--- src/App.java
+++ src/App.java
@@ -1,5 +1,40 @@
+/*
+ * Copyright (c) 2021, Nicolas Pierre, Eva Epoy, Jules Nicolas-Thouvenin. All rights reserved.
+ *
+ */
+
+/**
+ * Application
+ */
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+
+	public static void main(String[] args) {
+		
+		System.out.println("IntervalSet mySet = new IntervalSet(new Interval(5,10));");
+		IntervalSet mySet = new IntervalSet(new Interval(5,10));
+	
+		// mySet.enableVerbose(); // For debug only
+		mySet.printIntervals();
+		
+		System.out.println("mySet.remove(7);");
+		mySet.remove(7);
+		mySet.printIntervals();
+		
+		System.out.println("mySet.add(12);");
+		mySet.add(12);
+		mySet.printIntervals();
+		
+		System.out.println("mySet.add(11);");
+		mySet.add(11);
+		mySet.printIntervals();
+		
+		System.out.println("mySet.add(4);");
+		mySet.add(4);
+		mySet.printIntervals();
+		
+		System.out.println("mySet.remove(11);");
+		mySet.remove(11);
+		mySet.printIntervals();
+	}
+
 }
